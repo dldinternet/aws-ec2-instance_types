84224aeed09f4635b8390c6ee6b08dc10f600849
Christo DeLange <opscode@dldinternet.com>
2013-10-14 16:53:16 -0400

    Fix bug which parsed the two tables as one

--- lib/dldinternet/aws/ec2/instance_types.rb
+++ lib/dldinternet/aws/ec2/instance_types.rb
@@ -1,6 +1,7 @@
 module DLDInternet
   module AWS
     module EC2
+      class Error < StandardError ; end
       HEADINGS = [
           :instance_family,         # [0]
           :instance_type,           # [1]
@@ -12,9 +13,22 @@ module DLDInternet
           :EBS_optimized_available, # [7]
           :network_performance      # [8]
       ]
+      HEADINGS_CPU = [
+          :instance_family,			    # [0]
+          :instance_type,				    # [1]
+          :processor_arch,			    # [2]
+          :vCPU,						        # [3]
+          :ECU,						          # [4]
+          :physical_processor,	    # [5]
+          :Intel_AES_NI,				    # [6]
+          :Intel_AVX,						    # [7]
+          :Intel_Turbo,					    # [8]
+
+      ]
 
       attr_reader :instance_types
 
+      # ---------------------------------------------------------------------------------------------------------------
       def getInstanceTypes(options={})
         unless @instance_types
           @instance_types = {}
@@ -30,34 +44,43 @@ module DLDInternet
 
           require "nokogiri"
           nk = Nokogiri::HTML(page.body)
-          table = nk.css("div#yui-main div.yui-b table")
-          raise AWSEC2Error.new "Cannot find instance type table" unless table.is_a?(Nokogiri::XML::NodeSet)
-          rows = table.search('tr')[0..-1]
-          head = rows.shift
+          div = nk.css("div#yui-main div.yui-b")
+          tables = div.search('table')
 
-          cols = head.search('td').collect { |td|
-            text = td.text.to_s
-            text = text.gsub(%r/(\r?\n)+/, ' ').strip
-            CGI.unescapeHTML(text)
-          }
-
-          @instance_types[:headings] = {}
-          (0..cols.size-1).map{|i| @instance_types[:headings][HEADINGS[i]] = cols[i] }
-
-          @instance_types[:details] = []
-          rows.each do |row|
-
-            cells = row.search('td').collect { |td|
-              CGI.unescapeHTML(td.text.to_s.gsub(%r/(\r?\n)+/, ' ').strip.gsub('32-bit or64-bit', '32-bit or 64-bit'))
-            }
-            raise StandardError.new "This row does not have the same number of cells as the table header: #{row.text.to_s.strip}" unless cells.size == cols.size
-            instance = {}
-            (0..cells.size-1).map{|i| instance[HEADINGS[i]] = cells[i] }
-            @instance_types[:details] << instance
-
-          end
+          @instance_types[:instance_type_details] = scrapeTable(HEADINGS,     tables[0])
+          @instance_types[:processor_details]     = scrapeTable(HEADINGS_CPU, tables[1])
         end
         @instance_types
+      end
+
+      # ---------------------------------------------------------------------------------------------------------------
+      def scrapeTable(cHeadings,table)
+        raise Error.new "Cannot find instance type table" unless table.is_a?(Nokogiri::XML::Element)
+        rows = table.search('tr')[0..-1]
+        head = rows.shift
+
+        cols = head.search('td').collect { |td|
+          text = td.text.to_s
+          text = text.gsub(%r/(\r?\n)+/, ' ').strip
+          CGI.unescapeHTML(text)
+        }
+        instance_types = {
+            :headings => {},
+            :details  => []
+        }
+        (0..cols.size-1).map { |i| instance_types[:headings][cHeadings[i]] = cols[i] }
+
+        rows.each do |row|
+
+          cells = row.search('td').collect { |td|
+            CGI.unescapeHTML(td.text.to_s.gsub(%r/(\r?\n)+/, ' ').strip.gsub('32-bit or64-bit', '32-bit or 64-bit'))
+          }
+          raise StandardError.new "This row does not have the same number of cells as the table header: #{row.text.to_s.strip}" unless cells.size == cols.size
+          instance = {}
+          (0..cells.size-1).map { |i| instance[cHeadings[i]] = cells[i] }
+          instance_types[:details] << instance
+        end
+        instance_types
       end
     end
   end
